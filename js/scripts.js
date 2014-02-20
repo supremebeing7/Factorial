f9e3e8c7df95584301f941b82a8cbc5e4af8dd11
Alex Dinnel & Mark Lehman <student@epicodus.com>
2014-02-20 13:48:06 -0800

    counter for negatives and decimals

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,40 @@
 var factorial = function(number) {
   if (number === 0) {
     return 1;
-  };
-
-  if (number >= 1) {
-    console.log(number);
+  } else if (number >= 1 && number % 1 === 0) {
     return number * factorial(number -1);
+  }  
+    else {
+  return false;
   };
 };
+
+
+$(document).ready(function() {
+  $('form#factorials').submit(function(event) {
+    var numbers = ($('input#numbers').val());
+    var result = factorial(numbers);
+
+    $('#result').show();
+    $('.results').text(result);
+    this.reset();
+    event.preventDefault();
+  });
+});
+
+
+
+
+
+// $(document).ready(function() {
+//   $("").submit(function(event) {
+//     var phrase = $('input#phrase').val();
+
+//     var newPhrase = titleCase(phrase);
+
+//     $(".finalResult").text(newPhrase);
+//     $("#finalResult").show();
+//     this.reset();
+//     event.preventDefault();
+//   });
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -6,5 +6,13 @@ describe('factorial', function() {
   it('computes factorial of a number using recursion', function() {
     factorial(10).should.equal(3628800);
   });
+
+  it('Gives error when trying to compute factorial of a negative number using recursion', function() {
+    factorial(-1).should.equal(false);
+  });
+
+  it('Return false if given a decimal', function() {
+    factorial(1.5).should.equal(false);
+  })
 });
 
